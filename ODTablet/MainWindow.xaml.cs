8f62919638b956803fb855eace43d92999245b90
Marinho Moreira <fmarinho.rodrigues@gmail.com>
2014-07-14 12:16:39 -0600

    Zoom mode only. Libs available in libs.

---
+++ ODTablet/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+// ArcGIS
+using ESRI.ArcGIS.Client;
+
+using SOD_CS_Library;
+
+namespace ODTablet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SOD SoD;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConfigureSoD();
+            ConfigureDevice();
+            RegisterSoDEvents();
+        }
+
+
+        private void ConfigureSoD()
+        {
+            // Configure and instantiate SOD object
+            string address = "beastwin.marinhomoreira.com";
+            int port = 3000;
+            SoD = new SOD(address, port);
+        }
+
+        private void ConfigureDevice()
+        {
+            // Configure device with its dimensions (mm), location in physical space (X, Y, Z in meters, from sensor), orientation (degrees), Field Of View (FOV. degrees) and name
+            double widthInMM = 750
+                , heightInMM = 500
+                , locationX = -2
+                , locationY = 3
+                , locationZ = 1;
+            string deviceType = "WallDisplay";
+            bool stationary = true;
+            SoD.ownDevice.SetDeviceInformation(widthInMM, heightInMM, locationX, locationY, locationZ, deviceType, stationary);
+            SoD.ownDevice.orientation = 300;
+            SoD.ownDevice.FOV = 180;
+
+            // Name and ID of device - displayed in Locator
+            // TODO: Future: possible to look for devices using name, instead of ID.
+            SoD.ownDevice.ID = "2";
+            SoD.ownDevice.name = "MAIN_WALLDISPLAY";
+        }
+
+        private void RegisterSoDEvents()
+        {
+            // register for 'connect' event with io server
+            SoD.socket.On("connect", (data) =>
+            {
+                Console.WriteLine("\r\nConnected...");
+                Console.WriteLine("Registering with server...\r\n");
+                SoD.RegisterDevice();  //register the device with server everytime it connects or re-connects
+            });
+
+            SoD.socket.On("string", (data) =>
+            {
+                Dictionary<string, dynamic> parsedMessage = SoD.ParseMessageIntoDictionary(data);
+                Console.WriteLine("Received string: " + parsedMessage["data"]);
+            });
+
+            // make the socket.io connection
+            SoD.SocketConnect();
+        }
+
+        private void MyMap_ExtentChanging(object sender, ExtentEventArgs e)
+        {
+            //Console.WriteLine(e.NewExtent.ToString());
+            //send extent
+            string stringToSend = e.NewExtent.ToString();
+            SoD.SendStringToDevices(stringToSend, "all");
+        }
+    }
+}
